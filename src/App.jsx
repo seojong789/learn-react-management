745b8a75b7929b87d1334a72b9dfd45db03c8f65
Seo Jong-Pil <seojong789@naver.com>
2024-01-24 16:12:00 +0900

    REFACTOR: 사이드바에 프로젝트 선택 기능, 프로젝트 제거 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import ProjectsSidebar from './components/ProjectsSidebar';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
+import SelectedProject from './components/SelectedProject';
 
 const App = () => {
-  const [projectsState, setProjectState] = useState({
+  const [projectsState, setProjectsState] = useState({
     // 왜 null이 아니라 undefined?
     // null은 새로운 프로젝트를 추가하고 싶을 때, 사용하고
     // undefined는 추가한 새 프로젝트가 없거나 아무런 프로젝트도 선택하지 않았을 때 사용
@@ -12,8 +13,17 @@ const App = () => {
     projects: [],
   });
 
+  const handleSelectProject = (id) => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  };
+
   const handleStartAddProject = () => {
-    setProjectState((prevState) => {
+    setProjectsState((prevState) => {
       return {
         ...prevState,
         // undefined -> null : 이제 새 프로젝트를 추가한다는 의미.
@@ -23,7 +33,7 @@ const App = () => {
   };
 
   const handleCancelAddProject = () => {
-    setProjectState((prevState) => {
+    setProjectsState((prevState) => {
       return {
         ...prevState,
         selectedProjectId: undefined,
@@ -37,7 +47,7 @@ const App = () => {
       id: Math.random(), // id는 중복되면 안되서 좋은 방법이 아니지만, 연습용 코드임.
     };
 
-    setProjectState((prevState) => {
+    setProjectsState((prevState) => {
       return {
         ...prevState,
         selectedProjectId: undefined,
@@ -46,7 +56,26 @@ const App = () => {
     });
   };
 
-  let content;
+  const handleDeleteProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => {
+          // 지울 id에 해당하는 project를 배열에서 제거하는 구문임. (filter에서 false가 반환되면 해당 요소를 제외한 나머지 배열이 반환됨.)
+          project.id !== prevState.selectedProjectId;
+        }),
+      };
+    });
+  };
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
   if (projectsState.selectedProjectId === null) {
     // 새 프로젝트 추가
     content = (
@@ -65,6 +94,8 @@ const App = () => {
       <ProjectsSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
